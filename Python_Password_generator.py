c09d7e572cda31563d7649cd14403bba046ae5bd
JoaquinRodriguez2006 <joaquienrodriguez760@gmail.com>
2022-01-03 12:44:35 -0300

    Add Python Password Generator - P1

---
+++ Python_Password_generator.py
@@ -0,0 +1,11 @@
+import random
+
+lower = "abcdefghijklmnñopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "[]{}+-?¿!¡|*,;.:_"
+
+all = lower + upper + numbers + symbols
+length = 16
+password = "".join(random.sample(all,length))
+print(password)
